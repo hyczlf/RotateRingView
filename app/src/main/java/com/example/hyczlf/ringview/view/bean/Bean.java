cce9423d5ddeedafe5713233cbe5a68b95aa4283
hyczlf <lifan_bupt@163.com>
2017-12-20 17:26:21 +0800

    test3

---
+++ app/src/main/java/com/example/hyczlf/ringview/view/bean/Bean.java
@@ -0,0 +1,34 @@
+package com.example.hyczlf.ringview.view.bean;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * 项目名：  RotateRingView
+ * 包名：    com.example.hyczlf.ringview.view.bean
+ * 文件名:   Bean
+ *
+ * @author: hyczlf
+ * 创建时间:  2017/12/19 16:40
+ * 描述：    ImageView和TextView绑定
+ */
+public class Bean {
+    private ImageView iv;
+    private TextView tv;
+
+    public ImageView getIv() {
+        return iv;
+    }
+
+    public void setIv(ImageView iv) {
+        this.iv = iv;
+    }
+
+    public TextView getTv() {
+        return tv;
+    }
+
+    public void setTv(TextView tv) {
+        this.tv = tv;
+    }
+}
